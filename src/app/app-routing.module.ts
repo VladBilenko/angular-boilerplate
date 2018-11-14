7e71d2ec86fadc7cf8eee8ad9d12f4c0e21fe497
VladBilenko <vlad.bilenko.se@gmail.com>
2018-11-14 16:14:44 +0200

    add quality tools

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/template', pathMatch: 'full' },
@@ -9,6 +9,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,14 @@
 import { CommonModule } from '@angular/common';
-import {NgModule, Optional, SkipSelf} from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   declarations: [],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
-      throw new Error(
-        'Core module is already loaded. Import it in the AppModule only'
-      );
+      throw new Error('Core module is already loaded. Import it in the AppModule only');
     }
   }
 }
